2912c7334ef48e607a4a6c478ee2a41073be8bcc
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-04-28 22:28:28 +0700

    Update config

--- NeighborlyAPI/getPosts/__init__.py
+++ NeighborlyAPI/getPosts/__init__.py
@@ -12,7 +12,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         url = "mongodb://duongnq9-project2-cosmosdb-acc:gjKNOD7FD6SaBYJM2mt8gCOjDUEXdBmEUXMswMI4T8zkLobfEhfPI29q4AVp2ae05hofX6pEM5H38oEzZ69iyA==@duongnq9-project2-cosmosdb-acc.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@duongnq9-project2-cosmosdb-acc@"  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['duongnq9-project2-mongodb']
         collection = database['posts']
 
         result = collection.find({})
